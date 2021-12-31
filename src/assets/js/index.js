dac9c19641a45b19493a1632407e84d37c7dcddc
giangntse150746 <76811950+giangntse150746@users.noreply.github.com>
2021-12-31 09:59:33 +0700

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  const now = getNow();
+  console.log(now);
+  const newYear = new Date('2021-12-31T17:00:00.0000z');
+  console.log(newYear);
+  if (now < newYear)
+    window.location.href = 'count-down.html';
+  else
+    window.location.href = 'happy-new-year.html';
+
+});
